85402540ef7c5411893e571a7d422001225a43e9
RCRMS Devs <33639728+rcrms@users.noreply.github.com>
2017-12-04 22:38:29 -0500

    Add files via upload
    Even more modularized. The main html file - testBed.html - is now much more condensed. getSources.js shouldn't have to change anytime in the near future, while firebaseSources.html may, if Firebase releases newer versions of their JS sources.

---
+++ getSources.js
@@ -0,0 +1,29 @@
+
+function supportsImports() {
+    return 'import' in document.createElement('link');
+}
+
+if (!supportsImports()) {
+    // Good to go!
+    console.log('it is supported');
+
+    var link = document.querySelector('link[rel="import"]');
+    var content = link.import;
+
+    var app = content.querySelector('#app');
+    var auth = content.querySelector('#auth');
+    var firebase = content.querySelector('#firebase');
+    var database = content.querySelector('#database');
+
+    var div = document.getElementById('scriptTarget');
+
+    div.appendChild(app.cloneNode(true));
+    div.appendChild(auth.cloneNode(true));
+    div.appendChild(firebase.cloneNode(true));
+    div.appendChild(database.cloneNode(true));
+} else {
+    // Use other libraries/require systems to load files.
+    console.log('it is NOT supported');
+}
+
+    
