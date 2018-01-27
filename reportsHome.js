7cd0ddc03e720083d574c4dfb7d8bf80c0596d4f
RCRMS Devs <33639728+rcrms@users.noreply.github.com>
2018-01-27 15:02:31 -0500

    Reports Page
    updated most of the logic. provides option to create a csv instead of a google doc.

--- reportsHome.js
+++ reportsHome.js
@@ -1,8 +1,7 @@
-var formData = [];
 var myUser;
 
 // Client ID and API key from the Developer Console
-var CLIENT_ID = '';
+var CLIENT_ID = '795179805624-e6n5vspsihs6fitkg11rhal3s7ttgh2c.apps.googleusercontent.com';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://script.googleapis.com/$discovery/rest?version=v1"];
@@ -25,15 +24,22 @@ function onSignIn(googleUser){
 }
 
 function submitForm(){
-    if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
-        //user is NOT signed in -> sign them in then call API
-        gapi.auth2.getAuthInstance().signIn().then(callMyAPI, 
-            function handleError(e){
-                console.log('Google sign in error!', e);
-            });
-    } else{
-        //user is signed in
-        callMyAPI();
+    var csv = document.getElementById('csvCheckbox').checked;
+    if(csv){
+        //user DOES want a csv
+        createCSV();
+    } else {
+        //user does NOT want a csv
+        if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
+            //user is NOT signed in -> sign them in then call API
+            gapi.auth2.getAuthInstance().signIn().then(callMyAPI, 
+                function handleError(e){
+                    console.log('Google sign in error!', e);
+                });
+        } else{
+            //user is signed in
+            callMyAPI();
+        }
     }
 }
 /* SIGN OUT
@@ -49,68 +55,105 @@ function submitForm(){
 </script>
 
 */
-function callMyAPI(){
-    document.getElementById("groupByLabel").classList.remove("missingInfo");
-    document.getElementById("fromDateLabel").classList.remove("missingInfo");
-    document.getElementById("toDateLabel").classList.remove("missingInfo");
-    document.getElementById("complaintTypeLabel").classList.remove("missingInfo");
-
-    var radioBtnPressed, datesGiven, complaintGiven;
-
-    var radios = document.getElementsByName('groupBy');
-    for (var i = 0, length = radios.length; i < length; i++)
-    {
-        if (radios[i].checked)
-        {
-            //found the checked radio button
-            formData["groupBy"] = radios[i].id;
-            radioBtnPressed = true;
-            break;
-        }
-    }
-
-    pothole     = document.getElementById("pothole");
-    snow        = document.getElementById("snow");
-    obstruction = document.getElementById("obstruction");
-    fromDate    = document.getElementById("fromDate");
-    toDate      = document.getElementById("toDate");
-
-    if(pothole.checked || snow.checked || obstruction.checked){
-            complaintGiven = true;
-    }
-    if(fromDate.value != "" && toDate.value != ""){
-        datesGiven = true;
-    }
-
-    if(radioBtnPressed && datesGiven && complaintGiven){
-        //all data given, create report
-        formData["pothole"] = pothole.checked;
-        formData["snow"] = snow.checked;
-        formData["obstruction"] = obstruction.checked;
-
-        formData["fromDate"] = fromDate.value;
-        formData["toDate"] = toDate.value;
-
-        //CALL API HERE
-        callScriptFunction();
-
-    } else{//missing some info, narrow it down
-        if(!radioBtnPressed){
-            document.getElementById("groupByLabel").classList.add("missingInfo");
-        }
-        if(!datesGiven && fromDate.value == ""){
-            document.getElementById("fromDateLabel").classList.add("missingInfo");
-        }
-        if(!datesGiven && toDate.value == ""){
-            document.getElementById("toDateLabel").classList.add("missingInfo");
-        }
-        if(!complaintGiven){
-            document.getElementById("complaintTypeLabel").classList.add("missingInfo");
-        }
-
-    }
-
+function createCSV(){
+    var formData = getFormData();
     console.log(formData);
+    if(formData.county && formData.dataType && formData.fromDate && formData.groupBy && formData.toDate){//ready to query firebase
+        //call getCSVData cloud function
+            //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+        //parse http request response
+            //https://www.kirupa.com/html5/making_http_requests_js.htm
+        //download file for user
+    }
+}
+function getFormData(){
+
+    var datePicker1 = document.getElementById('fromDate');
+    var datePicker2 = document.getElementById('toDate');
+    var dataTypeLabel = document.getElementById('dataTypeLabel');
+    var groupByLabel = document.getElementById('groupByLabel');
+    var countyLabel = document.getElementById('countyLabel');
+
+    //reset all error indications
+    datePicker1.classList.remove('missingInfo');
+    datePicker2.classList.remove('missingInfo');
+    dataTypeLabel.classList.remove('missingInfo');
+    groupByLabel.classList.remove('missingInfo');
+    countyLabel.classList.remove('missingInfo');
+
+    var dataType, groupBy;
+    //get data type from radio buttons
+    if(document.getElementById('dataTypeResolved').checked){
+        dataType = "resolved";
+    } else if(document.getElementById('dataTypeUnresolved').checked){
+        dataType = "unresolved";
+    } else if(document.getElementById('dataTypeAll').checked){
+        dataType = "all";
+    }
+    //get group by data from radio buttons
+    if(document.getElementById('groupByDay').checked){
+        groupBy = "day";
+    } else if(document.getElementById('groupByWeek').checked){
+        groupBy = "week";
+    } else if(document.getElementById('groupByMonth').checked){
+        groupBy = "month";
+    }
+    //get form data
+    var dropDown = document.getElementById('countySelect');
+    var county = dropDown.options[dropDown.selectedIndex].text;
+    var fromDate = datePicker1.value;//get date string
+    var toDate = datePicker2.value;//get date string
+
+    //check for errors for current submit
+    if(!dataType){//data type group of radio buttons
+        dataTypeLabel.classList.add('missingInfo');
+    }
+    if(!groupBy){//group by group of radio buttons
+        groupByLabel.classList.add('missingInfo');
+    }
+    if(!county){//county drop down menu
+        countyLabel.classList.add('missingInfo');
+    }
+        //dates must be between the years 2000 and 2199 inclusivee
+        //must match YYYY-MM-DD or YYYY/MM/DD
+    var dateRegex = /^2[0|1]\d{2}[\/\-](0[1-9]|1[012])[\/\-](0[1-9]|[12][0-9]|3[01])$/
+
+    if(!dateRegex.test(fromDate)){
+        datePicker1.classList.add('missingInfo');
+        fromDate = null;
+    } else {
+        fromDate = fromDate.split('-');//make array
+        var tempDate = fromDate[0];//hold year
+        fromDate.shift();//remove year from fromDate[0]
+        fromDate.push(tempDate);//put year at end
+        fromDate = fromDate.join('/');//bring back to string
+    }
+    if(!dateRegex.test(toDate)){
+        datePicker2.classList.add('missingInfo');
+        toDate = null;
+    } else {
+        toDate = toDate.split('-');//make array
+        var tempDate = toDate[0];//hold year
+        toDate.shift();//remove year from fromDate[0]
+        toDate.push(tempDate);//put year at end
+        toDate = toDate.join('/');//bring back to string
+    }
+    //dates are now in format of 'MM/DD/YYYY' to match format on firebase
+
+    return {
+        dataType: dataType,
+        groupBy: groupBy,
+        county: county,
+        fromDate: fromDate,
+        toDate: toDate
+    }
+}
+function callMyAPI(){
+    var formData = getFormData();
+    console.log(formData);
+    if(formData.county && formData.dataType && formData.fromDate && formData.groupBy && formData.toDate){//ready to query firebase
+        callScriptFunction(formData);
+    }
 }
 
 /**
@@ -158,8 +201,8 @@ function updateSigninStatus(isSignedIn) {
 /**
  * Load the API and make an API call.
  */
-function callScriptFunction() {
-    var scriptId = "";                    
+function callScriptFunction(formData) {
+    var scriptId = "1opnMbOWoORI4iGfhWl9UPJGDxdYzQp1NUqTJmPX43ZGZezK5LJdV6kNg";
                     
     // Call the Execution API run method
     //   'scriptId' is the URL parameter that states what script to run
@@ -172,7 +215,6 @@ function callScriptFunction() {
         'resource': {
             'function': 'alterTemplate',
             'parameters':[
-                'dude wheres my file',
                 formData
             ]
         }
